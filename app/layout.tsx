7f7ee33225b7ab8f4c97fd8266dc4a25ba85de0b
Loyd Cose <loydcose.01@gmail.com>
2023-05-21 03:44:57 +0800

    adds favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import "./globals.css"
 import { Roboto_Mono } from "next/font/google"
+import Head from "next/head"
 
 const roboto_mono = Roboto_Mono({ subsets: ["latin"] })
 
@@ -16,10 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.svg" sizes="any" />
       <body className={roboto_mono.className + " bg-slate-900 text-slate-200"}>
         <main className="min-h-screen grid-place-items-center mx-auto w-[85%] max-w-[1206px] py-16">
           <h1 className="font-bold text-2xl mb-6 md:mb-12 flex items-center gap-3">
-            <Image src="/favicon.svg" alt="logo" width={70} height={70} className="w-[35px]"/>
+            <Image
+              src="/favicon.svg"
+              alt="logo"
+              width={70}
+              height={70}
+              className="w-[35px]"
+            />
             <span>Pokedex</span>
           </h1>
           {children}
